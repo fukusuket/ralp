47d732cf1fd459565549e7360e5cbb2976657a7a
fukusuket <fukusuke.takahashi@gmail.com>
2022-01-22 16:02:57 +0900

    extract method.

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,25 @@ use nom::character::complete::char;
 use nom::IResult;
 use nom::sequence::delimited;
 
-fn main() {
-    let f = File::create("out.csv").unwrap();
-    let mut wtr = WriterBuilder::new().quote_style(QuoteStyle::Always).from_writer(BufWriter::new(f));
-    let _ = wtr.write_record(&["time(utc)", "time(local)", "remote_host", "http_method", "http_status", "bytes", "request_url", "referer", "user_agent", "http_version", "remote log name", "remote user"]);
-    for result in BufReader::new(File::open("access_log").unwrap()).lines() {
-        let s = result.unwrap();
-        let (s, ip) = until_space(&s).unwrap_or_default();
-        let (s, u1) = until_space(&s[1..]).unwrap_or_default();
-        let (s, u2) = until_space(&s[1..]).unwrap_or_default();
-        let (s, t) = pair('[', &s[1..], ']').unwrap_or_default();
-        let t = DateTime::parse_from_str(t, "%d/%b/%Y:%H:%M:%S %z").unwrap();
-        let (s, url) = pair('"', &s[1..], '"').unwrap_or_default();
-        let v: Vec<String> = url.split_whitespace().map(|w| w.parse().ok().unwrap_or_default()).collect();
-        let (method, url, version) = parse_request_first_line(&v);
-        let (s, status) = until_space(&s[1..]).unwrap_or_default();
-        let (s, bytes) = until_space(&s[1..]).unwrap_or_default();
-        let (s, referer) = pair('"', &s[1..], '"').unwrap_or_default();
-        let (_, user_agent) = pair('"', &s[1..], '"').unwrap_or_default();
-        let _ = wtr.write_record(&[&t.naive_utc().to_string(), &t.to_string(), ip, method, status, bytes, url, referer, user_agent, version, u1, u2]);
-    }
-    let _ = wtr.flush();
+fn parse(l:&str) -> [&String; 12] {
+    // TODO error handling.
+    let (s, ip) = until_space(&s).unwrap_or_default();
+    let (s, u1) = until_space(&s[1..]).unwrap_or_default();
+    let (s, u2) = until_space(&s[1..]).unwrap_or_default();
+    let (s, t) = pair('[', &s[1..], ']').unwrap_or_default();
+    let t = DateTime::parse_from_str(t, "%d/%b/%Y:%H:%M:%S %z").unwrap();
+    let (s, url) = pair('"', &s[1..], '"').unwrap_or_default();
+    let v: Vec<String> = url.split_whitespace().map(|w| w.parse().ok().unwrap_or_default()).collect();
+    let (method, url, version) = parse_request_first_line(&v);
+    let (s, status) = until_space(&s[1..]).unwrap_or_default();
+    let (s, bytes) = until_space(&s[1..]).unwrap_or_default();
+    let (s, referer) = pair('"', &s[1..], '"').unwrap_or_default();
+    let (_, user_agent) = pair('"', &s[1..], '"').unwrap_or_default();
+    [&t.naive_utc().to_string(), &t.to_string(), ip, method, status, bytes, url, referer, user_agent, version, u1, u2]
 }
 
 fn pair(c1: char, input: &str, c2: char) -> IResult<&str, &str> {
-    delimited(char(c1),
-              is_not(String::from(c2).as_str()),
-              char(c2))(input)
+    delimited(char(c1), is_not(String::from(c2).as_str()), char(c2))(input)
 }
 
 fn until_space(input: &str) -> IResult<&str, &str> {
@@ -42,12 +34,20 @@ fn until_space(input: &str) -> IResult<&str, &str> {
 }
 
 fn parse_request_first_line(v: &Vec<String>) -> (&str, &str, &str) {
-    if v.len() == 3 {
-        return (&v[0], &v[1], &v[2])
-    }
-    ("-", "-", "-")
+    if v.len() == 3 { (&v[0], &v[1], &v[2]) } else { ("-", "-", "-") }
 }
 
+fn main() {
+    let f = File::create("out.csv").unwrap();
+    let mut wtr = WriterBuilder::new().quote_style(QuoteStyle::Always).from_writer(BufWriter::new(f));
+    let _ = wtr.write_record(&["time(utc)", "time(local)", "remote_host", "http_method", "http_status", "bytes", "request_url", "referer", "user_agent", "http_version", "remote log name", "remote user"]);
+    for result in BufReader::new(File::open("access_log").unwrap()).lines() {
+        let s = result.unwrap();
+        let r = parse(&s);
+        let _ = wtr.write_record(&r);
+    }
+    let _ = wtr.flush();
+}
 
 
 
